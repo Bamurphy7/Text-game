644029053df04a6f376ff163c0cb40df0a428432
Bmurphy666 <56304004+Bmurphy666@users.noreply.github.com>
2020-03-20 13:31:10 +0000

    monsters

--- Health.cpp
+++ Health.cpp
@@ -1,1 +1,82 @@
+#inlcude <iostream>
+#include <stdio.h>
+#include <string>
 
+class Monster_names :
+{
+
+	void Glarh;
+	void Yeur;
+	void Guu;
+	void Trazn;
+	void anbu;
+	void jire;
+
+}
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Here is " + Monster_names<< endl;
+	cin >> Monster_names >> endl;
+
+}
+
+
+
+void Monster_names::Glarh() {
+
+	<< cout "Health-50xp";
+	<< cout "Power-22xp";
+	<< cout "Speed- 7xp";
+	<< cout "Weight- 190kh";
+
+
+}
+
+
+void Yeur() {
+
+
+	<< cout "Health- 80xp";
+	<< cout "Power- 14xp";
+	<< cout "Speed- 12xp";
+	<< cout "Weight- 99kg";
+
+
+}
+
+void Guu(); {
+
+
+	<< cout "Health-50xp";
+	<< cout "Power- 2xp";
+	<< cout "Speed- 7xp";
+	<< cout "Weight- 70kg";
+}
+void Trazn() {
+
+	<< cout "Health- 32xp";
+	<< cout "Power- 32xp";
+	<< cout "Speed- 13xp";
+	<< cout "Weight- 50kg";
+
+}
+void Anbu() {
+
+	<< cout "Health- 8xp";
+	<< cout "Power- 46xp";
+	<< cout "Speed- 2xp";
+	<< cout "Weight- 300kg";
+
+}
+
+void Jire() {
+
+	<< cout "Health- 89xp";
+	<< cout "Power- 17xp";
+	<< cout "Speed- 9xp";
+	<< cout "Weight- 70kg";
+}
